0c9005ae51f766f042c3b2c14cab8247c615545d
slooi <s.looi2@outlook.com>
2019-12-23 04:10:55 +1300

    Seperate vertices and color, remove comments

--- webgl.js
+++ webgl.js
@@ -29,22 +29,7 @@ gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 const program = buildProgram()
 gl.useProgram(program)
 
-
-
-// vertices
-const vertices = new Float32Array([
-//	X		Y				R, G, B
-		-0.5, 0.0,	0,0,0	,
-		0.5, 0.0,		1,0,0,
-		0.0, 0.5,		0,1,0
-])
-
-// vertex Buffer
-const vertexBuffer = gl.createBuffer()
-gl.bindBuffer(gl.ARRAY_BUFFER,vertexBuffer)
-gl.bufferData(gl.ARRAY_BUFFER,vertices,gl.STATIC_DRAW)
-
-// attributes
+// Attribute Locations
 let attribLocations = []
 for(let i=0;i<gl.getProgramParameter(program,gl.ACTIVE_ATTRIBUTES);i++){
 	const attribName = gl.getActiveAttrib(program,i).name
@@ -52,33 +37,57 @@ for(let i=0;i<gl.getProgramParameter(program,gl.ACTIVE_ATTRIBUTES);i++){
 }
 console.log('attribLocations',attribLocations)
 
-// Attributes
+
+// Vertices
+const vertices = new Float32Array([
+//	X		Y			
+		-0.5, 0.0,
+		0.5, 0.0,	
+		0.0, 0.5
+])
+
+// Vertex Buffer
+const vertexBuffer = gl.createBuffer()
+gl.bindBuffer(gl.ARRAY_BUFFER,vertexBuffer)
+gl.bufferData(gl.ARRAY_BUFFER,vertices,gl.STATIC_DRAW)
+
+// Vertex Attribute Pointer
 gl.vertexAttribPointer(
 	attribLocations.a_VertexPositions,
 	2,
 	gl.FLOAT,
 	false,
-	5*Float32Array.BYTES_PER_ELEMENT,
+	2*Float32Array.BYTES_PER_ELEMENT,
 	0
 )
 gl.enableVertexAttribArray(attribLocations.a_VertexPositions)
+
+// Colors
+const colors = new Float32Array([
+	//	R, G, B
+	0,0,0	,
+	1,0,0,
+	0,1,0
+])
+
+// Color Buffer
+const colorBuffer = gl.createBuffer()
+gl.bindBuffer(gl.ARRAY_BUFFER,colorBuffer)
+gl.bufferData(gl.ARRAY_BUFFER,colors,gl.STATIC_DRAW)
+
+// Color Attribute Pointer
 gl.vertexAttribPointer(
 	attribLocations.a_Color,
 	3,
 	gl.FLOAT,
 	false,
-	5*Float32Array.BYTES_PER_ELEMENT,
-	2*Float32Array.BYTES_PER_ELEMENT
+	3*Float32Array.BYTES_PER_ELEMENT,
+	0
 )
 gl.enableVertexAttribArray(attribLocations.a_Color)
 
-// gl.drawArrays(gl.POINTS,0,vertices.length/2)
+gl.drawArrays(gl.POINTS,0,vertices.length/2)
 gl.drawArrays(gl.TRIANGLES,0,vertices.length/2)
-// const logl.getAttribLocation()
-// gl.vertexAttribPointer()
-
-// drawing
-// gl.drawArrays(gl.POINTS,)
 
 
 function buildShader(type,shaderSource){
